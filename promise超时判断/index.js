e7b738c174182bbc562ec883e0f21e5ff0af0588
Uni <1025687605@qq.com>
2018-04-14 21:06:32 +0800

    first commit

---
+++ promise超时判断/index.js
@@ -0,0 +1,36 @@
+/**
+利用Promise.race来进行超时处理
+**/
+function delay(miliseconds) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, miliseconds);
+	})
+}
+function timeoutPromise(promise, miliseconds) {
+	var timeout = delay(miliseconds).then(() => {
+		throw new Error(`request timed out after ${seconds}ms`)
+	})
+	return Promise.race([promise, timeout]);
+}
+function request(url, method) {
+	return new Promise((resolve, reject) => {
+		var xhr = new XMLHttpRequest();
+		xhr.open(url, method, true);
+		xhr.send();
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				if(xhr.status == 200) {
+					try {
+						var response = xhr.responseText;
+						resolve(response);
+					} catch(e) {
+						reject(e)
+					}
+				} else {
+					reject(new Error(xhr.statusText));
+				}	
+			}
+		}
+	})
+}
+timeoutPromise(request(url), 5000).then(()=>{}).then(()=>{});
